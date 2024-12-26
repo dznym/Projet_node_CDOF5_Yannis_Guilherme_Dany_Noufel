2171ff1d9dc4a24f8ba1cc5e1df05e44249a4484
dznym <danmo10492@gmail.com>
2024-12-26 20:39:17 +0100

    ajout de la connection à mongoDB

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log('Listening on : ${PORT}'))
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log('Connected To MongoDB'))
+  .catch((err) => console.log(err))
+
+app.listen(PORT, () => console.log(`Listening on : ${PORT}`))
