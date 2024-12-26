d8ead907288e4e063ee833c1381572a0fe99c50b
dznym <danmo10492@gmail.com>
2024-12-26 23:14:47 +0100

    base du frontend react

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+<div className="container">
+<h1>ToDo App</h1>
+<div className="top">
+  <input type="text" placeholder="Add ToDos..."/>
+</div>
+</div>
+    </div>
+  );
+}
+
+export default App;
