34013e7a9ac79f71fd629672994e4be28564322e
dznym <danmo10492@gmail.com>
2024-12-27 16:28:50 +0100

    ajout component ToDo.tsx pour la visualisation des ToDos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-
+import ToDo from "./components/ToDo.tsx";
 
 function App() {
   return (
     <div className="App">
-<div className="container">
-<h1>ToDo App</h1>
-<div className="top">
-  <input type="text" placeholder="Add ToDos..."/>
-</div>
-</div>
+      <div className="container">
+        <h1>ToDo App</h1>
+        <div className="top">
+          <input type="text" placeholder="Add ToDos..."/>
+          <div className="add">Add</div>
+          
+        </div>
+        <div className="list">
+          <ToDo text="test"/>
+        </div>
+      </div>
     </div>
   );
 }
