27bf17b71c6f3295f2b9d553161b49f8d715bd97
dznym <danmo10492@gmail.com>
2024-12-27 16:50:29 +0100

    ajout de cors pour lier le back et le front

--- backend/Server.ts
+++ backend/Server.ts
@@ -5,11 +5,12 @@ import todoRoutes from './routes/ToDoRoutes';
 
 
 dotenv.config();
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
