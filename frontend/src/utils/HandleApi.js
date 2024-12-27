46c303ecdce4c605312d3a22c976f586eff662ce
dznym <danmo10492@gmail.com>
2024-12-27 17:25:51 +0100

    ajout de la fonctionnalité update des todos

--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -19,6 +19,19 @@ const addToDo = (text, setText, setToDo) => {
         setText("")
         getAllToDo(setToDo)
     })
+    .catch((err)=> console.log(err))
+
 }
 
-export {getAllToDo, addToDo}
+
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+    axios
+    .post(`${baseUrl}/update`, {_id: toDoId, text})
+    .then((data)=> {
+        setText("")
+        setIsUpdating(false)
+        getAllToDo(setToDo)
+    })
+    .catch((err)=> console.log(err))
+}
+export {getAllToDo, addToDo, updateToDo}
