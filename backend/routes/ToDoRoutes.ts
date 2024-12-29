827bbcdb6297d0fe61f19b0ee4ed06975c7d3576
dznym <136904976+dznym@users.noreply.github.com>
2024-12-30 00:18:06 +0100

    Update ToDoRoutes.ts to implement swagger

--- backend/routes/ToDoRoutes.ts
+++ backend/routes/ToDoRoutes.ts
@@ -3,9 +3,52 @@ import { getToDo, saveToDo, updateToDo, deleteToDo } from "../controllers/ToDoCo
 
 const router: Router = express.Router();
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Get all ToDos
+ *     description: Retrieve a list of ToDos
+ *     responses:
+ *       200:
+ *         description: A list of ToDos
+ */
 router.get('/', getToDo);
+
+/**
+ * @swagger
+ * /save:
+ *   post:
+ *     summary: Save a new ToDo
+ *     description: Add a new ToDo item
+ *     responses:
+ *       200:
+ *         description: ToDo saved successfully
+ */
 router.post('/save', saveToDo);
+
+/**
+ * @swagger
+ * /update:
+ *   post:
+ *     summary: Update a ToDo
+ *     description: Update an existing ToDo item
+ *     responses:
+ *       200:
+ *         description: ToDo updated successfully
+ */
 router.post('/update', updateToDo);
-router.post('/delete', deleteToDo)
+
+/**
+ * @swagger
+ * /delete:
+ *   post:
+ *     summary: Delete a ToDo
+ *     description: Remove a ToDo item
+ *     responses:
+ *       200:
+ *         description: ToDo deleted successfully
+ */
+router.post('/delete', deleteToDo);
 
 export default router;
