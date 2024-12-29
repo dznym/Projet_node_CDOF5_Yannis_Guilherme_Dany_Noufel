4ba56cf6917169325ae192884a72c54a501c90ae
dznym <136904976+dznym@users.noreply.github.com>
2024-12-30 00:18:49 +0100

    Update Server.ts for swagger

--- backend/Server.ts
+++ backend/Server.ts
@@ -8,13 +8,12 @@ dotenv.config();
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 50000;
 app.use(cors());
 app.use(express.json());
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// Routes
 app.use('/', todoRoutes);
 
 // MongoDB connection
